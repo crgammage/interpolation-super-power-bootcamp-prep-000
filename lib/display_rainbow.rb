87a9eb4c3c33fb59ad3c54520b3c841be54ba88c
masterful-array-3850 <crgammage@gmail.com>
2020-04-13 16:48:41 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 def display_rainbow(colors)
-  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[3]}"
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}"
 end
